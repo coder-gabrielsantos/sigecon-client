8258fbda124b435deaeef5d3e9a18ca2af8f75de
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-11-23 14:52:13 -0300

    small user interface changes

--- src/services/extractorService.js
+++ src/services/extractorService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const extractorApi = axios.create({
-  baseURL: import.meta.env.EXTRACTOR_URL,
+  baseURL: import.meta.env.VITE_EXTRACTOR_URL,
 });
 
 export async function extractContractTable(file, onUploadProgress) {
